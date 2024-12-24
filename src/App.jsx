a1b098478cea20aa199d5de5b7779087337d2044
Quddus <tg53rt@gmail.com>
2024-12-25 00:00:33 +0500

    initilized

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {Button} from '@nextui-org/react';
+import Nav from './layouts/Nav';
+function App() {
+  return (
+    <div className="App">
+      <Button>ello</Button>
+    </div>
+  );
+}
+export default App;
