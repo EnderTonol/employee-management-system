493fabf354df1da3198c1e5c27bc2e1b862442ab
Quddus <tg53rt3@gmail.com>
2025-02-01 20:48:29 +0500

    loader setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'; 
 import React, { Suspense } from 'react';
+import { Spinner } from '@heroui/react';
 const Home = React.lazy(()=> import('./components/Home'));
 function App() {
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="flex h-[100vh] items-center justifiy-center"><Spinner color='primary'/><p className='font-mono font-bold'>Please Wait</p></div>}>
     <Home />
     </Suspense>
     </>
